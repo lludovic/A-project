e7710bfef22286e80cd5ba8b78b88969526fa140
jnorb <jnorblin@xebia.fr>
2017-01-09 15:24:41 +0100

    modifié:         src/main/java/com/xebia/git/CA.java

--- src/main/java/com/xebia/git/CA.java
+++ src/main/java/com/xebia/git/CA.java
@@ -2,13 +2,13 @@ package com.xebia.git;
 
 public class CA {
 
-    private String x;
+    private String y;
 
-    public String getX() {
-        return x;
+    public String getY() {
+        return y;
     }
 
-    public void setX(String x) {
-        this.x = x;
+    public void setY(String y) {
+        this.y = y;
     }
 }
