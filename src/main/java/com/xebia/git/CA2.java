bf2e2e18d3b0696509c8c109fec7a524ef507743
jnorb <jnorblin@xebia.fr>
2017-01-09 15:02:58 +0100

    nouveau fichier: src/main/java/com/xebia/git/CA2.java

---
+++ src/main/java/com/xebia/git/CA2.java
@@ -0,0 +1,14 @@
+package com.xebia.git;
+
+public class CA2 {
+
+    private String field2;
+
+    public String getField() {
+        return field2;
+    }
+
+    public void setField(String field2) {
+        this.field2 = field2;
+    }
+}
