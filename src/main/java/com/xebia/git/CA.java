e20d807353375de40416cc732a1fdf95186e531e
ludovicladeu <lladeu@xebia.fr>
2017-01-09 14:51:40 +0100

    Initial commit

---
+++ src/main/java/com/xebia/git/CA.java
@@ -0,0 +1,14 @@
+package com.xebia.git;
+
+public class CA {
+
+    private String field;
+
+    public String getField() {
+        return field;
+    }
+
+    public void setField(String field) {
+        this.field = field;
+    }
+}
