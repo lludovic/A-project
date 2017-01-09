772fdad3ef9f1e60534322fa9e95e1d33419d377
ludovicladeu <lladeu@xebia.fr>
2017-01-09 15:13:50 +0100

    add tasks on story

--- src/main/java/com/xebia/git/StoryOneService.java
+++ src/main/java/com/xebia/git/StoryOneService.java
@@ -1,4 +1,16 @@
 package com.xebia.git;
 
+import java.util.List;
+
 public class StoryOneService {
+
+    private List<String> task;
+
+    public List<String> getTask() {
+        return task;
+    }
+
+    public void setTask(List<String> task) {
+        this.task = task;
+    }
 }
